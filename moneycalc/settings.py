e270b73329ca7f7d8f2ce10c93b2e29980befa42
Deathik <smertelny@gmail.com>
2017-01-11 15:18:39 +0200

    min

--- moneycalc/settings.py
+++ moneycalc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z@vccq+2j3*bb3o21(rnw7b+9p76$73zc-)+9rg8(!duqyk!)='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://moneycalc.herokuapp.com/']
 
 
 # Application definition
